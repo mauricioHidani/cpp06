e62de9795b1e8145e57a5246035dee316c4f2e93
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-04-07 19:33:59 -0300

    fix: ex00 int converter

--- ex00/includes/IntConverter.hpp
+++ ex00/includes/IntConverter.hpp
@@ -2,31 +2,12 @@
 #pragma once
 
 #include <iostream>
-#include <exception>
 #include "IConverter.hpp"
 
 class IntConverter: public IConverter {
-private: 
-	int	_value;
-
-protected: 
-	IntConverter(const int& value);
-	IntConverter(const IntConverter& other);
-	IntConverter& operator=(const IntConverter& other);
-
 public: 
 	IntConverter(void);
 	~IntConverter(void);
 
-	int getValue(void) const;
-
 	void convertType(const std::string& value);
-
-public:
-	class ImpossibleException: public std::exception {
-	public:
-		virtual const char* what() const throw();
-	};
 };
-
-std::ostream& operator<<(std::ostream& out, const IntConverter& obj);
--- ex00/src/IntConverter.cpp
+++ ex00/src/IntConverter.cpp
@@ -1,38 +1,41 @@
 
 #include "IntConverter.hpp"
+#include <climits>
+#include <cmath>
+#include <cstdlib>
+#include <string>
 
-IntConverter::IntConverter(void)
-	:	_value(0) {
-}
-
-IntConverter::IntConverter(const IntConverter& other)
-	:	_value(other.getValue()) {
+IntConverter::IntConverter(void) {
 }
 
 IntConverter::~IntConverter(void) {
 }
 
-int IntConverter::getValue(void) const {
-	return _value;
-}
-
 void IntConverter::convertType(const std::string& value) {
+	std::string	impossibles[] = {
+		"nan", "nanf", 
+		"inf", "+inf", "-inf", 
+		"inff", "+inff", "-inff"
+	};
 
-}
+	char*	endptr = NULL;
+	double	res = 0;
 
-IntConverter& IntConverter::operator=(const IntConverter& other) {
-	if (this != &other) {
-		_value = other.getValue();
+	if (value.length() == 1 && !std::isdigit(value[0])) {
+		std::cout << "int: " << static_cast<int>(value[0]) << std::endl;
+		return ;
 	}
 
-	return *this;
-}
+	for (size_t i = 0; i < impossibles->length(); i++)
+		if (value.compare(impossibles[i]) == 0)
+			throw std::runtime_error("int: impossible");
+	
+	res = std::strtod(value.c_str(), &endptr);
+	if (*endptr != '\0' && value.length() > 0)
+		throw std::runtime_error("int: impossible");
 
-std::ostream& operator<<(std::ostream& out, const IntConverter& instance) {
-	out << "int: " << instance.getValue();
-	return out;
-}
+	if (res < INT_MIN || res > INT_MAX || std::isnan(res))
+		throw std::runtime_error("int: impossible");
 
-const char* IntConverter::ImpossibleException::what() const throw() {
-	return "int: impossible";
+	std::cout << "int: " << static_cast<int>(res) << std::endl;
 }
